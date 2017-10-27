cb7a4d43426356b1f1a6a09847cb2a0741c76e87
dfm114 <dfm114@ic.ac.uk>
2017-10-27 11:50:51 +0100

    Use inline functions
    Signed-off-by: dfm114 <dfm114@ic.ac.uk>

--- macros.h
+++ macros.h
@@ -1,14 +1,27 @@
 #ifndef SAT_MACROS_H
 #define SAT_MACROS_H
+#include <stdio.h>
+#include <stdarg.h>
 
 #define BOUND_CHECK(len, idx) assert(idx < len)
 #define UNUSED(x)  x __attribute__((unused))
-#define LOG_FATAL(x, ...) LOG_DEBUG(x, ##__VA_ARGS__); \
-                            abort();
+static inline void LOG_FATAL(const char* fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  vfprintf(stderr, "%s:%d: Aborting execution", __FILE__, __LINE__);
+  vfprintf(stderr, fmt, args);
+  va_end(args);
+  abort();
+}
 #ifdef DEBUG
-#define LOG_DEBUG(x, ...) fprintf(stderr, x, ##__VA_ARGS__)
+static inline void LOG_DEBUG(const char* fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  vfprintf(stderr, fmt, args);
+  va_end(args);
+}
 #else
-#define LOG_DEBUG(x, ...)
+static inline void LOG_DEBUG(const char* UNUSED(fmt), UNUSED(...)) { }
 #endif
 
 #endif //SAT_MACROS_H
